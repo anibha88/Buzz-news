5bba5e8746de133ecfc25265d32d5cc7142cd784
anibha88 <anibha88@gmail.com>
2015-11-19 11:08:22 +0530

    test commit

--- buzz-news/app/controllers/application_controller.rb
+++ buzz-news/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # adorable
   protect_from_forgery with: :exception
 end
